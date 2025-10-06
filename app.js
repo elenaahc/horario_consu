995c0364c194c0f6e01dd95717def44a1e8872ab
elenaahc <67713830+elenaahc@users.noreply.github.com>
2025-10-06 17:22:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,44 +2,45 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const schedules = {
     lunes: [
-      { hora: "14:15 - 14:55", actividad: "Intervenciones Educativas en Salud\n sala: S-03-P03-A", clase: "intervenciones" },
-      { hora: "14:56 - 15:35", actividad: "Intervenciones Educativas en Salud sala: S-03-P03-A", clase: "intervenciones" },
-      { hora: "15:46 - 16:25", actividad: "Bases Fisiopatológicas para el Cuidado sala: S-03-P03-A", clase: "bases-fisiopatologicas" },
-      { hora: "16:26 - 17:05", actividad: "Bases Fisiopatológicas para el Cuidado sala: S-03-P03-A", clase: "bases-fisiopatologicas" },
-      { hora: "17:15 - 17:55", actividad: "Bases Fisiopatológicas para el Cuidado sala: S-03-P03-A", clase: "bases-fisiopatologicas" },
-      { hora: "17:56 - 18:35", actividad: "Bases Fisiopatológicas para el Cuidado sala: S-03-P03-A", clase: "bases2-fisiopatologicas" },
-      { hora: "18:40 - 19:20", actividad: "Bases Fisiopatológicas para el Cuidado sala: S-03-P03-A", clase: "bases2-fisiopatologicas" },
+      { hora: "14:15 - 14:55", actividad: "Intervenciones Educativas en Salud \n Sala: S-03-P03-A \n Docente: Valeska Norambuena", clase: "intervenciones" },
+      { hora: "14:56 - 15:35", actividad: "Intervenciones Educativas en Salud \n Sala: S-03-P03-A \n Docente: Valeska Norambuena", clase: "intervenciones" },
+      { hora: "15:46 - 16:25", actividad: "Bases Fisiopatológicas para el Cuidado \n Sala: S-03-P03-A \n Docente: Rayen Oliva", clase: "bases-fisiopatologicas" },
+      { hora: "16:26 - 17:05", actividad: "Bases Fisiopatológicas para el Cuidado \n Sala: S-03-P03-A \n Docente: Rayen Oliva", clase: "bases-fisiopatologicas" },
+      { hora: "17:15 - 17:55", actividad: "Bases Fisiopatológicas para el Cuidado \n Sala: S-03-P03-A \n Docente: Rayen Oliva", clase: "bases-fisiopatologicas" },
+      { hora: "17:56 - 18:35", actividad: "Bases Fisiopatológicas para el Cuidado \n Sala: S-03-P03-A \n Docente: Rayen Oliva", clase: "bases2-fisiopatologicas" },
+      { hora: "18:40 - 19:20", actividad: "Bases Fisiopatológicas para el Cuidado \n Sala: S-03-P03-A \n Docente: Rayen Oliva", clase: "bases2-fisiopatologicas" },
     ],
     martes: [
-      { hora: "14:15 - 14:55", actividad: "Intervenciones Educativas en Salud sala: S-03-P03-A", clase: "intervenciones2" },
-      { hora: "14:56 - 15:35", actividad: "Intervenciones Educativas en Salud sala: S-03-P03-A", clase: "intervenciones2" },
-      { hora: "15:46 - 16:25", actividad: "Fundamentos en Salud Pública sala: S-03-P03-A", clase: "fundamentos-salud" },
-      { hora: "16:26 - 17:05", actividad: "Fundamentos en Salud Pública sala: S-03-P03-A", clase: "fundamentos-salud" },
-      { hora: "17:15 - 17:55", actividad: "Gestión del Cuidado en Salud Mental sala: S-03-P03-A", clase: "gestion-salud-mental" },
-      { hora: "17:56 - 18:35", actividad: "Gestión del Cuidado en Salud Mental sala: S-03-P03-A", clase: "gestion-salud-mental" },
-      { hora: "18:40 - 19:20", actividad: "Gestión del Cuidado en Salud Mental sala: S-03-P03-A", clase: "gestion-salud-mental" },
+      { hora: "14:15 - 14:55", actividad: "Intervenciones Educativas en Salud \n Sala: S-03-P03-A \n Docente: Valeska Norambuena", clase: "intervenciones2" },
+      { hora: "14:56 - 15:35", actividad: "Intervenciones Educativas en Salud \n Sala: S-03-P03-A \n Docente: Valeska Norambuena", clase: "intervenciones2" },
+      { hora: "15:46 - 16:25", actividad: "Fundamentos en Salud Pública \n Sala: S-03-P03-A \n Docente: Madison Cid", clase: "fundamentos-salud" },
+      { hora: "16:26 - 17:05", actividad: "Fundamentos en Salud Pública \n Sala: S-03-P03-A \n Docente: Madison Cid", clase: "fundamentos-salud" },
+      { hora: "17:15 - 17:55", actividad: "Gestión del Cuidado en Salud Mental \n Sala: S-03-P03-A \n Docente: Guillermo Ormeño", clase: "gestion-salud-mental" },
+      { hora: "17:56 - 18:35", actividad: "Gestión del Cuidado en Salud Mental \n Sala: S-03-P03-A \n Docente: Guillermo Ormeño", clase: "gestion-salud-mental" },
+      { hora: "18:40 - 19:20", actividad: "Gestión del Cuidado en Salud Mental \n Sala: S-03-P03-A \n Docente: Guillermo Ormeño", clase: "gestion-salud-mental" },
     ],
     miércoles: [
-    //   { hora: "14:15 - 14:55", actividad: "Inglés Básico I sala: S-03-P05-A", clase: "ingles-basico" },
-    //   { hora: "14:56 - 15:35", actividad: "Inglés Básico I sala: S-03-P05-A", clase: "ingles-basico" },
-      { hora: "15:46 - 16:25", actividad: "Fundamentos en Salud Pública sala: S-02-P05-A", clase: "fundamentos-salud" },
-      { hora: "16:26 - 17:05", actividad: "Fundamentos en Salud Pública sala: S-02-P05-A", clase: "fundamentos-salud" },
-      { hora: "17:15 - 17:55", actividad: "Fundamentos en Salud Pública sala: S-02-P05-A", clase: "fundamentos-salud" },
+    //   { hora: "14:15 - 14:55", actividad: "Inglés Básico I \n Sala: S-03-P05-A \n Docente: Valentina Mendoza", clase: "ingles-basico" },
+    //   { hora: "14:56 - 15:35", actividad: "Inglés Básico I \n Sala: S-03-P05-A \n Docente: Valentina Mendoza", clase: "ingles-basico" },
+      { hora: "15:46 - 16:25", actividad: "Fundamentos en Salud Pública \n Sala: S-02-P05-A \n Docente: Madison Cid", clase: "fundamentos-salud" },
+      { hora: "16:26 - 17:05", actividad: "Fundamentos en Salud Pública \n Sala: S-02-P05-A \n Docente: Madison Cid", clase: "fundamentos-salud" },
+      { hora: "17:15 - 17:55", actividad: "Fundamentos en Salud Pública \n Sala: S-02-P05-A \n Docente: Madison Cid", clase: "fundamentos-salud" },
     ],
     jueves: [
-      { hora: "14:15 - 14:55", actividad: "Proceso de Enfermería sala: L-07-P06-A", clase: "proceso-enfermeria" },
-      { hora: "14:56 - 15:35", actividad: "Proceso de Enfermería sala: L-07-P06-A", clase: "proceso-enfermeria" },
+      { hora: "14:15 - 14:55", actividad: "Proceso de Enfermería \n Sala: L-07-P06-A \n Docente: Isela Robertson", clase: "proceso-enfermeria" },
+      { hora: "14:56 - 15:35", actividad: "Proceso de Enfermería \n Sala: L-07-P06-A \n Docente: Isela Robertson", clase: "proceso-enfermeria" },
     ],
     viernes: [
-    //   { hora: "14:15 - 14:55", actividad: "Inglés Básico I sala: S-03-P05-A", clase: "ingles-basico" },
-    //   { hora: "14:56 - 15:35", actividad: "Inglés Básico I sala: S-03-P05-A", clase: "ingles-basico" },
-      { hora: "15:46 - 16:25", actividad: "Bases Fisiopatológicas para el Cuidado sala: S-03-P05-A", clase: "bases-fisiopatologicas" },
-      { hora: "16:26 - 17:05", actividad: "Bases Fisiopatológicas para el Cuidado sala: S-03-P05-A", clase: "bases-fisiopatologicas" },
-      { hora: "17:15 - 17:55", actividad: "Gestión del Cuidado en Salud Mental sala: S-03-P05-A", clase: "gestion2-salud-mental" },
-      { hora: "17:56 - 18:35", actividad: "Gestión del Cuidado en Salud Mental sala: S-03-P05-A", clase: "gestion2-salud-mental" },
+    //   { hora: "14:15 - 14:55", actividad: "Inglés Básico I \n Sala: S-03-P05-A \n Docente: Valentina Mendoza", clase: "ingles-basico" },
+    //   { hora: "14:56 - 15:35", actividad: "Inglés Básico I \n Sala: S-03-P05-A \n Docente: Valentina Mendoza", clase: "ingles-basico" },
+      { hora: "15:46 - 16:25", actividad: "Bases Fisiopatológicas para el Cuidado \n Sala: S-03-P05-A \n Docente: Rayen Oliva", clase: "bases-fisiopatologicas" },
+      { hora: "16:26 - 17:05", actividad: "Bases Fisiopatológicas para el Cuidado \n Sala: S-03-P05-A \n Docente: Rayen Oliva", clase: "bases-fisiopatologicas" },
+      { hora: "17:15 - 17:55", actividad: "Gestión del Cuidado en Salud Mental \n Sala: S-03-P05-A \n Docente: Guillermo Ormeño", clase: "gestion2-salud-mental" },
+      { hora: "17:56 - 18:35", actividad: "Gestión del Cuidado en Salud Mental \n Sala: S-03-P05-A \n Docente: Guillermo Ormeño", clase: "gestion2-salud-mental" },
     ],
   };
 
+  
   function updateSchedule() {
     const selectedDay = document.getElementById('daySelect').value;
     const scheduleTableBody = document.querySelector('#scheduleTable tbody');
